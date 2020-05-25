c916b4f439a8a320f390344e8b921f473fb1e730
amanda <albertsch8@gmail.com>
2020-05-25 16:28:06 -0700

    GET/POST for exercises and users

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-require('dotenv').config('.env');
+require('dotenv').config('.env'); //mongodb 
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -18,11 +18,13 @@ connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
 
-// const exercisesRouter = require('./routes/exercises');
-// const usersRouter = require('./routes/users');
+// ------- Routes --------
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
 
-// app.use('/exercises', exercisesRouter);
-// app.use('/users', usersRouter);
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+// ------
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
